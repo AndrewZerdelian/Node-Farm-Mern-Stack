c986ff8987f53192a7fe014375533cd2399e20d7
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-06-17 18:03:21 +0300

    Impord and export in js with require & module.exports for replaceTemplate...

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { error } = require("console");
 const fs = require("fs"); //same thing
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./moduels/replaceTemplate');
+//require('./modules/replaceTemplate');
 
 //import { readFileSync } from "fs";
 
@@ -90,22 +92,7 @@ const url = require("url");
 // });
 
 /////////////////////////////////////////////WEB API ////////////////////////////////////
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%productName%}/g, product.productName);
-  output = output.replace(/{%image%}/g, product.image);
-  output = output.replace(/{%price%}/g, product.price);
-  output = output.replace(/{%from%}/g, product.from);
-  output = output.replace(/{%nutrients%}/g, product.nutrients);
-  output = output.replace(/{%quantity%}/g, product.quantity);
-  output = output.replace(/{%description%}/g, product.description);
-  output = output.replace(/{%id%}/g, product.id);
 
-  if (!product.organic) {
-    output = output.replace(/{%not-organic%}/g, `not-organic`);
-  }
-
-  return output;
-};
 const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const Template_OverView = fs.readFileSync(
   `${__dirname}/templates/tamplet-overview.html`,
